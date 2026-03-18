205d1a346e4ebf91db47bcbecadbfa069bf63541
TonyBllvc <geroge.ekwemadu@gmail.com>
2026-03-19 00:35:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <main>
+     <h1> Hello World</h1>
+  </main>
+</template>
+
+<style scoped>
+
+main{
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+</style>
